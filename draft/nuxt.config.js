4afe06a15c2523ee0db8b98d0a6170284730aaa5
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-08 22:52:54 +0100

    use gh-pages

--- draft/nuxt.config.js
+++ draft/nuxt.config.js
@@ -16,10 +16,6 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ["~/assets/css/style"],
 
-  router: {
-    base: '/draft/dist/'
-  },
-
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
