ea3940ea9c1bb25a62cffb4a6cdf1086b8959e23
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-08 22:33:22 +0100

    https://stackoverflow.com/a/50944870/1070215

--- draft/nuxt.config.js
+++ draft/nuxt.config.js
@@ -16,6 +16,10 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ["~/assets/css/style"],
 
+  router: {
+    base: '/draft/dist/'
+  },
+
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
