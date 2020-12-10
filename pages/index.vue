6a68a9814a5a430d5fafcda39b6b7d48c08c7fb6
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-10 02:19:36 +0100

    footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
     <nuxt-content :document="presentazione" />
+    <client-only>
+      <footer class="footer">
+        <div class="float-right">{{ version }}</div>
+      </footer>
+    </client-only>
   </div>
 </template>
+<style >
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  background-color: #f5f5f5;
+
+  font-family: monospace;
+}
+</style>
 <script>
 export default {
   async asyncData({ $content }) {
@@ -10,6 +27,7 @@ export default {
 
     return {
       presentazione,
+      version: process.env.NUXT_ENV_CURRENT_GIT_SHA,
     };
   },
 };
