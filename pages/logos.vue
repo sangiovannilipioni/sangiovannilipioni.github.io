d60fcd69c82dc1328a39e21a361949045e4696ad
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-13 16:17:40 +0100

    Alchimie du code : délires I

--- pages/logos.vue
+++ pages/logos.vue
@@ -1,34 +1,48 @@
 <template>
   <div class="logos mx-auto">
-    <style>
-      .logos svg path {
-        stroke-width: {{strokeWidth}};
-      }
-    </style>
-    <div id="blah" class="d-flex">
-      <label class="d-inline-block flex-shrink-1" for="customRange1"
-        >Stroke Width: <span>{{ strokeWidth }}</span></label
-      >
-      &nbsp;
-      <input
-        class="custom-range w-100 d-inline-block"
-        type="range"
-        id="customRange1"
-        min="1"
-        max="160"
-        step="1"
-        v-model="strokeWidth"
-      />
-    </div>
+    <!-- https://github.com/nuxt/nuxt.js/issues/6645#issuecomment-550111141 -->
+    <component :is="'style'">
+      .logos svg path { stroke-width: {{ strokeWidth }}; } 
+      .logos svg > g {
+      transform: translate(-20px,-250px) {{ getMatrixForRotation(310, 535) }}; }
+    </component>
+    <b-form id="strokeWidthForm" class="form-inline">
+      <div class="blah d-flex w-100">
+        <label class="" for="strokeWidth">Stroke Width</label>
+        <b-form-input
+          class="d-inline-flex px-2"
+          type="range"
+          id="strokeWidth"
+          min="0"
+          max="128"
+          step="1"
+          v-model="strokeWidth"
+        />
+        <div class="feedback">{{ strokeWidth }}</div>
+      </div>
+      <div class="d-flex w-100">
+        <label class="" for="angle">Angle</label>
+        <b-form-input
+          class="px-2"
+          type="range"
+          id="angle"
+          min="0"
+          max="360"
+          step="1"
+          v-model="angle"
+        />
+        <div class="feedback">{{ angle }}</div>
+      </div>
+    </b-form>
     <div v-html="src" class="d-flex justify-content-center"></div>
   </div>
 </template>
 
 <style>
-#blah {
+.blah {
   white-space: nowrap;
 }
-label[for="customRange1"] span {
+.feedback {
   font-family: monospace;
   font-size: larger;
 }
@@ -39,12 +53,30 @@ export default {
   data() {
     return {
       strokeWidth: 20,
+      angle: 180,
     };
   },
   props: {
     name: {
       type: String,
       required: true,
+    },
+  },
+  methods: {
+    // https://stackoverflow.com/a/31488227/1070215
+    getMatrixForRotation(cx, cy) {
+      const an = this.angle;
+      var ca = Math.cos((an * Math.PI) / 180);
+      var sa = Math.sin((an * Math.PI) / 180);
+
+      var a = ca.toFixed(4);
+      var b = sa.toFixed(4);
+      var c = (-sa).toFixed(4);
+      var d = ca.toFixed(4);
+      var e = (-ca * cx + sa * cy + cx).toFixed(4);
+      var f = (-sa * cx - ca * cy + cy).toFixed(4);
+
+      return "matrix(" + [a, b, c, d, e, f].join(", ") + ")";
     },
   },
   computed: {
