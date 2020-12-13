b33410476af83b62fa5b0e716f8d57a45c7a5585
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-13 12:13:22 +0100

    logo dynamic stroke width

--- pages/logos.vue
+++ pages/logos.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="logos mx-auto">
-    <div v-html="src"></div>
+    <style>
+      .logos svg path {
+        stroke-width: {{strokeWidth}};
+      }
+    </style>
+    <div id="blah" class="d-flex">
+      <label class="d-inline-block flex-shrink-1" for="customRange1"
+        >Stroke Width: <span>{{ strokeWidth }}</span></label
+      >
+      &nbsp;
+      <input
+        class="custom-range w-100 d-inline-block"
+        type="range"
+        id="customRange1"
+        min="1"
+        max="160"
+        step="1"
+        v-model="strokeWidth"
+      />
+    </div>
+    <div v-html="src" class="d-flex justify-content-center"></div>
   </div>
 </template>
+
 <style>
-.logos svg path {
-  stroke-width: 24;
+#blah {
+  white-space: nowrap;
+}
+label[for="customRange1"] span {
+  font-family: monospace;
+  font-size: larger;
 }
 </style>
 <script>
 export default {
   name: "Icon",
+  data() {
+    return {
+      strokeWidth: 20,
+    };
+  },
   props: {
     name: {
       type: String,
@@ -19,7 +49,7 @@ export default {
   },
   computed: {
     src() {
-      const src = require(`assets/svg/Logo_qr_code.svg?raw`);
+      const src = require(`assets/svg/Logo_qr_code_2.svg?raw`);
       return src;
     },
   },
