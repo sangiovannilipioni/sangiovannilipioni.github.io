a4bbfc095c940d1273c2451892830dd30b6ed50f
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-16 10:53:35 +0100

    fixed swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -108,8 +108,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: "~/plugins/vue-pdf.js", mode: "client" },
-    { src: "./plugins/vue-awesome-swiper.js", ssr: false }
+    { src: "~/plugins/vue-pdf.client.js", mode: "client" },
+    { src: "./plugins/vue-awesome-swiper.client.js", mode: "client" }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/slides.vue
+++ pages/slides.vue
@@ -1,33 +1,39 @@
 <template>
-  <client-only>
-    <swiper class="swiper" :options="swiperOption">
-      <swiper-slide>
-        Press
-        <code class="code">left</code>
-        or
-        <code class="code">right</code>
-        to control slide
-      </swiper-slide>
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide 4</swiper-slide>
-      <swiper-slide>Slide 5</swiper-slide>
-      <swiper-slide>Slide 6</swiper-slide>
-      <swiper-slide>Slide 7</swiper-slide>
-      <swiper-slide>Slide 8</swiper-slide>
-      <swiper-slide>Slide 9</swiper-slide>
-      <swiper-slide>Slide 10</swiper-slide>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-      <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
-  </client-only>
+  <div>
+    <client-only>
+      <swiper class="swiper" :options="swiperOption">
+        <swiper-slide>
+          Press
+          <code class="code">left</code>
+          or
+          <code class="code">right</code>
+          to control slide
+        </swiper-slide>
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <swiper-slide>Slide 6</swiper-slide>
+        <swiper-slide>Slide 7</swiper-slide>
+        <swiper-slide>Slide 8</swiper-slide>
+        <swiper-slide>Slide 9</swiper-slide>
+        <swiper-slide>Slide 10</swiper-slide>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </client-only>
+  </div>
 </template>
 
 <script>
+// https://github.com/nolimits4web/swiper/issues/3768#issuecomment-680926562
+import Swiper from 'swiper/swiper-bundle.esm'
+
 export default {
   name: "swiper-example-navigation",
+  ssr: false,
   title: "Navigation",
   data() {
     return {
@@ -42,7 +48,7 @@ export default {
         },
         pagination: {
           el: ".swiper-pagination",
-          type: "progressbar",
+          // type: "progressbar",
         },
         loop: false,
       },
@@ -52,14 +58,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "swiper/swiper-bundle.css";
 .swiper {
   height: auto;
 
   .swiper-slide {
     text-align: center;
-    height: 300px;
-    line-height: 300px;
+    height: 520px;
+    line-height: 520px;
+    background: #ddd;
   }
 }
 </style>
