88e19506533fbe3b553d0c34b5af91f8e66b5e8a
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-02-21 09:12:57 +0100

    new error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,32 @@
 <!-- https://www.damirscorner.com/blog/posts/20200904-ErrorHandlingInNuxtjs.html -->
 <template>
-  <div>
-    <h2 v-if="error.statusCode === 404">Page not found</h2>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+  <div class="container mt-4">
+    <h4>
+      <b>{{ error.statusCode }}.</b> That’s an error.
+    </h4>
+    <div v-if="error.statusCode === 404" class="mb-4">
+      The requested URL <span class="text-danger">{{ slug }}</span> was not found on this server.
+      <div class="text-muted">That's all I know.</div>
+    </div>
+    <div v-if="error.statusCode === 500" class="mb-4">
+      <b>Internal Server Error</b> The server has encountered a situation it doesn't know how to handle.
+      <div class="text-muted">That's all I can say.</div>
+    </div>
+    <NuxtLink to="/">Go back to home page</NuxtLink>
+    <div>
+      <img src="/svg/Logo_sad.svg" class="float-right img-fluid" style="max-width: 25vh">
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['error'],
-    layout: 'error' // you can set a custom layout for the error page
-  }
+export default {
+  props: ["error"],
+  layout: "error", // you can set a custom layout for the error page
+  data() {
+    return {
+      slug: this.$nuxt.$route.path,
+    };
+  },
+};
 </script>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -14,7 +14,10 @@
 export default {
   async asyncData(context) {
     const { $content, params, app, route, redirect } = context;
-    const article = await $content(`${app.i18n.locale}/articles`, params.slug).fetch();
+    const article = await $content(
+      `${app.i18n.locale}/articles`,
+      params.slug
+    ).fetch();
     return { article };
   },
 };
