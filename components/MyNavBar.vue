9f1cec7c3f5b005eef7717c1f500d0e9f0d0f3ba
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-07 15:56:59 +0200

    mieux que rien

--- components/MyNavBar.vue
+++ components/MyNavBar.vue
@@ -1,87 +1,100 @@
 <template>
-  <client-only>
-    <b-navbar toggleable="md" class="container" style="padding: 0 1rem">
-      <b-navbar-brand :to="localePath('/')">
-        <Logo :class="isHomePage?'home':'not-home'"/>
-      </b-navbar-brand>
+  <b-navbar toggleable="md" class="container" style="padding: 0 1rem">
+    <b-navbar-brand :to="localePath('/')">
+      <Logo :class="isHomePage ? 'home' : 'not-home'" />
+    </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" style="text-align: center; border: 1px dotted gray; border-radius: 0.25rem;" is-nav>
-        <b-navbar-nav toggleable="sm" style="flex: auto">
-          <b-nav-item :to="localePath('/slides')">{{
-            $t("presentation")
-          }}</b-nav-item>
+    <b-collapse
+      id="nav-collapse"
+      style="
+        text-align: center;
+        border: 1px dotted gray;
+        border-radius: 0.25rem;
+      "
+      is-nav
+    >
+      <b-navbar-nav toggleable="sm" style="flex: auto">
+        <b-nav-item :to="localePath('/slides')">{{
+          $t("presentation")
+        }}</b-nav-item>
 
+        <b-nav-item-dropdown>
+          <template slot="button-content">{{ $t("patrimonio") }}</template>
+          <b-dropdown-item :to="localePath('/patrimonio')">{{
+            $t("patrimonio")
+          }}</b-dropdown-item>
+          <b-dropdown-item :to="localePath('/videos2')">{{
+            $t("media")
+          }}</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item :to="localePath('/collab_da')">{{
+            $t("collab_da")
+          }}</b-dropdown-item>
+        </b-nav-item-dropdown>
 
-          <b-nav-item-dropdown>
-            <template slot="button-content">{{ $t("patrimonio") }}</template>
-            <b-dropdown-item  :to="localePath('/patrimonio')">{{
-              $t("patrimonio")
+        <b-nav-item :to="localePath('/maiellaverde')">{{
+          $t("maiellaverde")
+        }}</b-nav-item>
+        <b-nav-item id="spy" href="#" class="flex-grow-1 text-muted" disabled
+          ><span v-if="$auth.$state.loggedIn">{{
+            $route.path
+          }}</span></b-nav-item
+        >
+
+        <locale-switcher />
+
+        <template v-if="$auth.$state.loggedIn">
+          <b-nav-item-dropdown :text="$auth.user ? $auth.user.name : ''" right>
+            <template slot="button-content"
+              ><b-img
+                v-if="$auth.user"
+                :src="$auth.user.picture.data.url"
+                class="mt-1"
+                rounded="circle"
+                width="30px"
+                height="30px"
+            /></template>
+            <b-dropdown-item :to="localePath('/secure')">{{
+              $t("gossip")
             }}</b-dropdown-item>
-            <b-dropdown-item  :to="localePath('/videos2')">{{
-              $t("media")
+            <b-dropdown-item :to="localePath('/logos')">{{
+              $t("impresa")
             }}</b-dropdown-item>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item  :to="localePath('/collab_da')">{{
-              $t("collab_da") 
-            }}</b-dropdown-item>
+            <div class="dropdown-divider"></div>
+            <b-dropdown-item @click="$auth.logout()">
+              <font-awesome-icon :icon="['fas', 'sign-out-alt']" />&nbsp;{{
+                $t("signOut")
+              }}
+            </b-dropdown-item>
           </b-nav-item-dropdown>
-          
-          
-          <b-nav-item :to="localePath('/maiellaverde')">{{
-            $t("maiellaverde") 
-          }}</b-nav-item>
-          <b-nav-item id="spy" href="#" class="flex-grow-1 text-muted" disabled><span v-if="$auth.$state.loggedIn" >{{ $route.path }}</span></b-nav-item>
-
-          <locale-switcher />
-
-          <template v-if="$auth.$state.loggedIn">
-            <b-nav-item-dropdown :text="$auth.user ? $auth.user.name : ''" right>
-              <template slot="button-content"
-                ><b-img
-                  v-if="$auth.user"
-                  :src="$auth.user.picture.data.url"
-                  class="mt-1"
-                  rounded="circle"
-                  width="30px"
-                  height="30px"
-              /></template>
-              <b-dropdown-item :to="localePath('/secure')">{{
-                $t("gossip")
-              }}</b-dropdown-item>
-              <b-dropdown-item :to="localePath('/logos')">{{
-                $t("impresa")
-              }}</b-dropdown-item>
-              <div class="dropdown-divider"></div>
-              <b-dropdown-item @click="$auth.logout()">
-                <font-awesome-icon :icon="['fas', 'sign-out-alt']" />&nbsp;{{
-                  $t("signOut")
-                }}
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-          </template>
-          <template v-else>
-            <b-nav-item :to="localePath('/login')" right>
-              <span style="white-space: nowrap;"
-                >{{
-                  $t("signIn")
-                }}<font-awesome-icon :icon="['fas', 'sign-in-alt']" style="margin-left: 0.5rem;" /></span
-              >
-            </b-nav-item>
-          </template>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </client-only>
+        </template>
+        <template v-else>
+          <b-nav-item :to="localePath('/login')" right>
+            <span style="white-space: nowrap"
+              >{{ $t("signIn")
+              }}<font-awesome-icon
+                :icon="['fas', 'sign-in-alt']"
+                style="margin-left: 0.5rem"
+            /></span>
+          </b-nav-item>
+        </template>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
 export default {
   computed: {
     isHomePage() {
-      return this.$route.path === '/' || this.$route.path === '/fr' || this.$route.path === '/en';
-    }
+      return (
+        this.$route.path === "/" ||
+        this.$route.path === "/fr" ||
+        this.$route.path === "/en"
+      );
+    },
   },
 };
 </script>
--- pages/units/_slug.vue
+++ pages/units/_slug.vue
@@ -4,26 +4,27 @@
       <font-awesome-icon :icon="['fas', 'arrow-left']" />
     </b-button>
     <!-- nuxt-content :document="unit" /-->
-    <div class="d-flex bd-highlight">
-      <div class="p-2 flex-fill bd-highlight" style="min-width: 30%;">
-        <div
-            v-for="text in texts"
-            v-bind:key="text"
-        >
-        <h4>{{ text }}</h4>
+    {{ slug }}
+    <div class="d-flex bd-highlight" v-if="theUnit">
+      <div class="p-2 flex-fill bd-highlight" style="min-width: 30%">
+        <div v-for="text in theUnit.texts" v-bind:key="text">
+          <h4>{{ text }}</h4>
         </div>
+        <b-img v-for="img in theUnit.imgs" v-bind:key="img" :src="`/foto/${img}`" fluid>
+        </b-img>
       </div>
       <div class="p-2 flex-fill bd-highlight">
         <b-carousel
+          v-if="theUnit.jpegs.length"
           id="carousel-1"
           controls
           indicators
-          style="color:red;"
+          style="color: red"
           no-animation
         >
           <b-carousel-slide
-            :img-src="jpeg"
-            v-for="jpeg in jpegs"
+            :img-src="`/foto/${jpeg}`"
+            v-for="jpeg in theUnit.jpegs"
             v-bind:key="jpeg"
           >
           </b-carousel-slide>
@@ -42,15 +43,14 @@
   background-image: none;
 }
 
-.carousel-control-next-icon:after
-{
-  content: '>';
+.carousel-control-next-icon:after {
+  content: ">";
   font-size: 55px;
   color: navy;
 }
 
 .carousel-control-prev-icon:after {
-  content: '<';
+  content: "<";
   font-size: 55px;
   color: navy;
 }
@@ -60,19 +60,36 @@
 export default {
   data() {
     return {
-      jpegs: [
-        "/foto/O1__O2-PianoSeminterrato.jpg",
-        "/foto/O1__O2-PianoTerra.jpg",
-        "/foto/O1__O2-ProspettoNord.jpg",
-        "/foto/O1__O2-ProspettoOvestEst.jpg",
-        "/foto/O1__O2-ProspettoSud.jpg",
-        "/foto/O1__O2-SezioneA.jpg",
-        "/foto/O1__O2-SezioneBCD.jpg",
-      ],
-      texts: [
-        "Via Roma, 37"
-      ]
+      units: {
+        A02: {
+          jpegs: [
+            "O1__O2-PianoPrimo.jpg",
+            "O1__O2-PianoSeminterrato.jpg",
+            "O1__O2-PianoTerra.jpg",
+            "O1__O2-ProspettoNord.jpg",
+            "O1__O2-ProspettoOvestEst.jpg",
+            "O1__O2-ProspettoSud.jpg",
+            "O1__O2-SezioneA.jpg",
+            "O1__O2-SezioneBCD.jpg",
+          ],
+          texts: ["Via Roma, 37"],
+          imgs: ["GOPR1232_light_2.jpg"],
+        },
+        G01: {
+          jpegs: [],
+          texts: ["Via Vicenne. 4"],
+          imgs: ["X2.jpg"],
+        },
+      },
     };
+  },
+  computed: {
+    slug() {
+      return this.$route.params.slug
+    },
+    theUnit() {
+      return this.units[this.$route.params.slug]
+    }
   },
   async asyncData({ $content, params, app, route, redirect }) {
     const unit = await $content(
