567aac7cafaf7f2a46f0d21b21e614b8d6a1a608
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-10 00:45:32 +0200

    data à peu près ok

--- pages/data/_slug.vue
+++ pages/data/_slug.vue
@@ -1,11 +1,11 @@
 <template>
   <article class="container-fluid">
     <b-tabs content-class="mt-3">
-      <b-tab v-for="(tab, index) in data" :key="index" :title="tab.sheet" active>
-        <table>
+      <b-tab v-for="(tab, index) in data" :key="index" :title="tab.sheet" :active="index==0">
+        <table class="table table-bordered table-striped table-hover table-sm table-responsive">
           <tr v-for="(row, index2) in tab.rows" :key="index2" >
-            <td >
-              {{ row }}
+            <td v-for="(col, index3) in toArray(tab.cols, row)" :key="index3">
+              {{ col }}
             </td>
           </tr>
         </table>
@@ -26,6 +26,25 @@ export default {
   },
   async fetch() {
     this.data = await fetch("/json/SintesiO2.json").then((res) => res.json());
+  },
+  methods: {
+    toArray(cols, row) {
+      let ret = []
+      let index = 0;
+      row.forEach(element => {
+        for (let i = index; i < element.col; i++) {
+          ret.push("");
+          index++;
+        }
+        ret.push(element.text);
+        index++;
+      });
+      for (let i = index; i <= cols; i++) {
+        ret.push("");
+        index++;
+      }
+      return ret;
+    },
   },
 };
 </script>
