d3df2f8f00ade67f6696e0eb050a62869f8ac981
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-10 01:12:02 +0200

    fetchOnServer false

--- pages/data/_slug.vue
+++ pages/data/_slug.vue
@@ -1,17 +1,33 @@
 <template>
-  <article class="container-fluid">
-    <b-tabs content-class="mt-3">
-      <b-tab v-for="(tab, index) in data" :key="index" :title="tab.sheet" :active="index==0">
-        <table class="table table-bordered table-striped table-hover table-sm table-responsive">
-          <tr v-for="(row, index2) in tab.rows" :key="index2" >
-            <td v-for="(col, index3) in toArray(tab.cols, row)" :key="index3">
-              {{ col }}
-            </td>
-          </tr>
-        </table>
-      </b-tab>
-    </b-tabs> 
-  </article>
+  <client-only>
+    <article class="container-fluid">
+      <b-tabs content-class="mt-3">
+        <b-tab
+          v-for="(tab, index) in data"
+          :key="index"
+          :title="tab.sheet"
+          :active="index == 0"
+        >
+          <table
+            class="
+              table
+              table-bordered
+              table-striped
+              table-hover
+              table-sm
+              table-responsive
+            "
+          >
+            <tr v-for="(row, index2) in tab.rows" :key="index2">
+              <td v-for="(col, index3) in toArray(tab.cols, row)" :key="index3">
+                {{ col }}
+              </td>
+            </tr>
+          </table>
+        </b-tab>
+      </b-tabs>
+    </article>
+  </client-only>
 </template>
 
 <style lang="scss">
@@ -27,11 +43,13 @@ export default {
   async fetch() {
     this.data = await fetch("/json/SintesiO2.json").then((res) => res.json());
   },
+  fetchOnServer: false,
+  fetchKey: 'dugenou',
   methods: {
     toArray(cols, row) {
-      let ret = []
+      let ret = [];
       let index = 0;
-      row.forEach(element => {
+      row.forEach((element) => {
         for (let i = index; i < element.col; i++) {
           ret.push("");
           index++;
