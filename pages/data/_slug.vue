936f5cd4351c37dec2e4d49dba7511a1152f2d77
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-18 12:53:01 +0200

    nice html for data

--- pages/data/_slug.vue
+++ pages/data/_slug.vue
@@ -2,25 +2,15 @@
   <client-only>
     <article class="container-fluid">
       <b-tabs content-class="mt-3">
-
         <b-tab
           v-for="(tab, index) in datum"
           :key="index"
           :title="tab.sheet"
           :active="index == 0"
         >
-          <table
-            class="
-              table
-              table-bordered
-              table-striped
-              table-hover
-              table-sm
-              table-responsive
-            "
-          >
-            <tr v-for="(row, index2) in tab.rows" :key="index2">
-              <td v-for="(col, index3) in toArray(tab.cols, row)" :key="index3">
+          <table class="table table-sm table-responsive table-borderless">
+            <tr v-for="(row, index2) in tab.rows" :key="index2" :style="row.style">
+              <td v-for="(col, index3) in toArray(tab.cols, row)" :key="index3" :style="col.style">
                 {{ col }}
               </td>
             </tr>
@@ -39,19 +29,48 @@ export default {
   data() {
     return {
       units: {
-        O02: {
-        },
-        G01: {
-        },
+        O02: {},
+        G01: {},
       },
     };
   },
   async fetch() {
-    this.units[this.$route.params.slug] = await fetch("/json/SintesiO2.json").then((res) => res.json());
+    const data = this.massageData(
+      await fetch("/json/SintesiO2.json").then((res) => res.json())
+    );
+    this.units[this.$route.params.slug] = data;
   },
   fetchOnServer: false,
-  fetchKey: 'dugenou',
+  fetchKey: "dugenou",
   methods: {
+    massageData(json) {
+      json.forEach((sheet) => {
+        // remove first row
+        sheet.rows.shift()
+
+        // for all rows, 
+        sheet.rows.forEach((row) => {
+          const firstcol = row[0];
+          // if first column is a numeric, style the row with gray background
+          if (firstcol && firstcol.col === "0" && !isNaN(firstcol.text) ) {
+            row.style = "background-color: #eee;"
+          }
+          // for all cols, 
+          row.forEach((col) => {
+            // if text is numeric and end with ".0", remove it
+            if (!isNaN(col.text) && col.text.endsWith('.0')) {
+              col.text = col.text.substring(0, col.text.length - 2);
+            }
+            if (col.text === "EDIFICIO IN AGGREGATO" || col.text === "MONOFAMILIARE") {
+              row.style = "color: #a42424;"
+            }
+          });
+
+        });
+        return
+      });
+      return json;
+    },
     toArray(cols, row) {
       let ret = [];
       let index = 0;
@@ -77,6 +96,6 @@ export default {
     datum() {
       return this.units[this.$route.params.slug];
     },
-  }
+  },
 };
 </script>
