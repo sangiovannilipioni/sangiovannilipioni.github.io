df051c0667578441762237bc0289ecdee29e16ab
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-22 15:34:32 +0200

    plus classieux

--- pages/data/_slug.vue
+++ pages/data/_slug.vue
@@ -128,7 +128,7 @@ export default {
           if (cell.col !== "0") {
             cell.text = parseFloat(cell.text).toFixed(2)
           }
-          cell.text = cell.text.replace(/\.0+$/, '').replace(/(\.[1-9]*)0+$/, '$1')
+          cell.text = cell.text.replace(/\.0+$/, "").replace(/(\.[1-9]*)0+$/, "$1")
         }
       }
 
@@ -140,38 +140,40 @@ export default {
         "04_dati_costrutt_CARENZE": "Caratteristiche costruttive solai"
       }
 
+      const fixTypos = {
+        "stato di conservaz": "stato di conservazione",
+        carenzestrutturali: "carenze strutturali"
+      }
+
       // remove all sheets that have no title
       json = json.filter((sheet) => sheetTitles[sheet.sheet])
 
       // for all sheets
       json.forEach((sheet) => {
-        // set id
+        // set id & title
         sheet.id = `_${sheet.sheet.replace(" ", "_")}`
-        // set title
         sheet.title = sheetTitles[sheet.sheet]
 
         // remove first row
         sheet.rows.shift()
 
+        // helpers
         let is_01_id_edificio = sheet.sheet === "01_id_edificio"
         let is_02_descriz_edificio = sheet.sheet === "02_descriz_edificio"
         let is_03_dati_metrici_AB = sheet.sheet === "03_dati_metrici_AB"
         let is_04_dati_costrutt_VERT_IQM = sheet.sheet === "04_dati_costrutt_VERT_IQM"
         let is_04_dati_costrutt_CARENZE = sheet.sheet === "04_dati_costrutt_CARENZE"
 
-        let ignoreNextRows = undefined
-        let nextRowColor = undefined
-        let nextColumnBackground = undefined
-
+        // the mythical breadcrum class
         let breadcrumb = {
           id: undefined,
           row: 0,
-          aliases: {
-            "4.2.1": "4.2.1",
-            "4.2.2": "4.2.2",
-            "4.4.2": "4.4.2_4.4.3",
-            "4.4.3": "4.4.2_4.4.3",
-            "4.4.4": "4.4.4"
+          jpegNameMap: {
+            "4.2.1": "4.2.1.jpg",
+            "4.2.2": "4.2.2.jpg",
+            "4.4.2": "4.4.2_4.4.3.jpg",
+            "4.4.3": "4.4.2_4.4.3.jpg",
+            "4.4.4": "4.4.4.jpg"
           },
           get level() {
             let ret = 0
@@ -181,48 +183,49 @@ export default {
             return ret
           },
           get jpeg() {
-            return this.aliases[this.id]
+            return this.jpegNameMap[this.id]
           }
         }
 
+        // declare row iteration flags
+        let ignoreNextRows = undefined
+        let nextRowColor = undefined
+        let nextColumnBackground = undefined
         let ignoreNextNextColumns = undefined
 
         // for all rows
         sheet.rows.forEach((row, rowindex) => {
-          let ignoreThisRow = false
           // hide ignored rows
-          if (ignoreNextRows === true) {
-            ignoreThisRow = true
-          }
+          let ignoreThisRow = ignoreNextRows === true
+
           // consume row color
           if (nextRowColor) {
             appendStyle(row, { color: nextRowColor })
             nextRowColor = undefined
           }
 
-          const fixTypos = {
-            "stato di conservaz": "stato di conservazione",
-            carenzestrutturali: "carenze strutturali"
-          }
-
+          // declare cell iteration flags
           let indirizzo = undefined
-          let appendImage = undefined
+          let backgroundImageCell = undefined
           let columnBackground = undefined
+
           // consume column background color
           if (nextColumnBackground) {
             columnBackground = nextColumnBackground
             nextColumnBackground = undefined
           }
+
           // for all cols
           row.forEach((cell, colindex) => {
             // for each column -------------------------------------------------
-
             // fix typos
             cell.text = fixTypos[cell.text] || cell.text
 
             // if first column is column zero, and is a numeric, style the row with gray background
             if (colindex === 0) {
               if (cell.col === "0" && !isNaN(cell.text.replace(".", ""))) {
+                ignoreThisRow = false
+
                 breadcrumb.id = cell.text
                 breadcrumb.row = 0
 
@@ -236,27 +239,26 @@ export default {
                   }
                 }
 
-                appendStyle(row, {
-                  backgroundColor: bgColor,
-                  display: "table.row"
-                })
+                appendStyle(row, { backgroundColor: bgColor })
               }
             }
 
             // format numbers
             formatNumber(cell)
 
-            const appendBackgroundImage = (rowspan) => {
+            // function to instantiate background image cell
+            const instantiateBackgroundImageCell = (rowspan) => {
               ignoreNextNextColumns = true
               if (breadcrumb.jpeg) {
-                appendImage = {
+                backgroundImageCell = {
                   col: "5",
                   rowspan: rowspan || "4",
                   text: "‌&zwnj;",
                   style:
-                    "position: relative; background: center / cover no-repeat url(/json/jpegs/" +
+                    "position: relative; background: center / cover no-repeat " +
+                    "url(/json/jpegs/" +
                     breadcrumb.jpeg +
-                    ".jpg)"
+                    ")"
                 }
               }
             }
@@ -266,7 +268,6 @@ export default {
               if (cell.text === "DATI CATASTALI") {
                 ignoreNextRows = true
                 ignoreThisRow = true
-                // appendStyle(row, { display: "none" })
               }
               if (indirizzo) {
                 this.title = cell.text
@@ -276,6 +277,7 @@ export default {
                 indirizzo = cell
               }
             }
+
             // special case 2 -------------------------------------------------
             else if (is_02_descriz_edificio) {
               // next row text will be red
@@ -289,10 +291,10 @@ export default {
               if (ignoreNextNextColumns && 4 < cell.col) {
                 appendStyle(cell, { display: "none" })
               }
+
               if (cell.col <= 3 && cell.text.match(/(codice id. elementi costruttivi|IQMv|IQMo,fp|IQMo,np)/g)) {
                 ignoreThisRow = true
               }
-
               if (cell.text.match(/pareti (interne|esterne)/g)) {
                 columnBackground = undefined
                 appendStyle(cell, { fontWeight: 600 })
@@ -300,15 +302,15 @@ export default {
               if (cell.text.match(/CATEGORIA/g)) {
                 ignoreThisRow = true
               }
-              if (cell.text.match(/A[1234]/g)) {
+              if (cell.text.match(/[ABC][1234]/g)) {
                 columnBackground = "#c6e0b3"
                 nextColumnBackground = "#eee"
                 appendStyle(cell, { fontWeight: 600 })
               }
               if (cell.text.match(/materiali/g)) {
-                appendBackgroundImage()
+                instantiateBackgroundImageCell()
               }
-              if (columnBackground && 1 <= cell.col && cell.col <= 3) {
+              if (columnBackground && 2 <= cell.col) {
                 if (0 < breadcrumb.row) {
                   appendStyle(cell, { backgroundColor: columnBackground })
                 }
@@ -338,14 +340,13 @@ export default {
                 ignoreNextRows = false
                 columnBackground = "#ffe699"
               }
-
-              // "ct" and "c" are bold
               if (breadcrumb.level === 3 && breadcrumb.row === 0) {
                 // next row background will be dark orange
                 nextColumnBackground = "#ffc000"
 
                 if (cell.col === "5") {
                   cell.col = "4"
+                  // "ct", "c", "-1", "0", etc. are bold
                   appendStyle(cell, { fontWeight: 600 })
                 }
               }
@@ -377,7 +378,7 @@ export default {
               }
 
               if (breadcrumb.row === 2) {
-                appendBackgroundImage("5")
+                instantiateBackgroundImageCell("5")
               }
             }
           }) // cells
@@ -397,7 +398,7 @@ export default {
             if (!(ignoreNextRows === true)) {
               breadcrumb.row = breadcrumb.row + 1
               if (is_04_dati_costrutt_CARENZE) {
-                // ignore rows after second row of third level breadcrumb until an all uppercase cell is found
+                // ignore rows after second row of third level breadcrumb (until an all uppercase cell is found)
                 if (breadcrumb.level === 3 && breadcrumb.row === 2) {
                   ignoreNextRows = true
                 }
@@ -405,9 +406,10 @@ export default {
             }
           }
 
-          if (appendImage) {
-            row.push(appendImage)
-            appendImage = undefined
+          // consume background image cell
+          if (backgroundImageCell) {
+            row.push(backgroundImageCell)
+            backgroundImageCell = undefined
           }
         }) // rows
       }) // sheets
