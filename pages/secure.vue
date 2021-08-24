1d61868b73d32757459c4ae649a0ce475aad501d
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-24 20:00:43 +0200

    more secure

--- pages/secure.vue
+++ pages/secure.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
-    <div role="alert" class="alert alert-warning">{{ this.$t('secureMessage')}}</div>
+    <div role="alert" class="alert alert-warning">{{ this.$t("secureMessage") }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  // middleware: ["auth"]
+  middleware: ["auth"]
 }
 </script>
