f21afc445abb50363d930ab24da31b1acaa32f29
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-09-08 22:20:40 +0200

    fail deploy if git not clean

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 # https://sangsoonam.github.io/2019/02/08/using-git-worktree-to-deploy-github-pages.html
+
+# git must be clean to deploy
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+  echo Working directory clean
+else 
+  # Uncommitted changes
+  echo Uncommitted changes
+  exit 1
+fi
+
 rm -rf deploy
 mkdir deploy
 cd deploy
