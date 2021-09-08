eeddadb85bb3cc637f15bbebc84c4a881e070045
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-09-08 15:21:09 +0200

    deploy script (Travis down)

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# /bin/bash
+set -ex
+# https://sangsoonam.github.io/2019/02/08/using-git-worktree-to-deploy-github-pages.html
+rm -rf deploy
+mkdir deploy
+cd deploy
+git clone https://github.com/sangiovannilipioni/sangiovannilipioni.github.io.git
+cd sangiovannilipioni.github.io
+cp ../../.env .
+yarn install
+yarn generate
+cd dist
+git init
+git add -A
+git commit -m 'chore: deploy'
+git push -f https://github.com/sangiovannilipioni/sangiovannilipioni.github.io.git master:gh-pages
+cd ../..
+rm -rf deploy
